4ca6f39c47469d4593b8381a184b218497c04989
Rofaidaz <hr_smida@esi.dz>
2019-12-28 13:13:47 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/signin', function () {
+    return view('signin');
+});
